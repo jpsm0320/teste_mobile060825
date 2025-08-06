fa70f1ea15c5cefc1d21c0fa10c815f5c74798b7
jpsm0320 <jpsalsamachado03@gmail.com>
2025-08-06 16:22:42 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 const CACHE_NAME = 'pwa-task-list-v1';
 const urlsToCache = [
-    '/',
-    '/index.html',
-    '/style.css',
-    '/manifest.json',
-    '/icons/icon-192x192.png',
-    '/icons/icon-512x512.png'
+    '/teste_mobile060825/',
+    '/teste_mobile060825/index.html',
+    '/teste_mobile060825/style.css',
+    '/teste_mobile060825/manifest.json',
+    '/teste_mobile060825/icons/icon-192x192.png',
+    '/teste_mobile060825/icons/icon-512x512.png'
 ];
 
 self.addEventListener('install', event => {
@@ -25,3 +25,4 @@ self.addEventListener('fetch', event => {
             })
     );
 });
+
